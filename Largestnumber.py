60e6887c5b77b3bedc60407bc6e332ef39f657ed
Pranjali-Malviya <167666150+Pranjali-Malviya@users.noreply.github.com>
2024-04-21 11:31:57 +0530

    Update Largestnumber.py

--- Largestnumber.py
+++ Largestnumber.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
+def largest_num(number_1,number_2,number_3):
+  l=[number_1,number_2,number_3]
+  l.sort()
+  return (l[2])
 
-number_1=int(input("Enter the 1st number:"))
-number_2=int(input("Enter the 2nd number:"))
-number_3=int(input("Enter the 3rd number:"))
-l=[number_1,number_2,number_3]
-l.sort()
-print("Largest number is", l[2])
+st.set_page_config(page_title="Largest Number")
+st.sidebar.title("Largest Number")
+form.st.sidebar.form(key="form", clear_on_submit=False)
+number_1=form.number.input("Enter the 1st number:",step=1)
+number_2=form.number.input("Enter the 2nd number:",step=1)
+number_3=form.number.input("Enter the 3rd number:",step=1)
+submitted=form.form_submit_button("Find the Largest")
+
+if submitted:
+  st.header('Result:')
+  result=largest_num(number_1,number_2,number_3)
+  st.Write(f'The largest number is {result}')
+else:
+  st.header('Result:')
+  st.write("Waiting for submiting")
