91c349970da88c35c038a557215c3e6c928af040
Pranjali-Malviya <167666150+Pranjali-Malviya@users.noreply.github.com>
2024-04-21 11:37:02 +0530

    Update Largestnumber.py

--- Largestnumber.py
+++ Largestnumber.py
@@ -1,22 +1,24 @@
-import streamlit as st
-
-def largest_num(number_1,number_2,number_3):
-  l=[number_1,number_2,number_3]
-  l.sort()
-  return (l[2])
-
-st.set_page_config(page_title="Largest Number")
-st.sidebar.title("Largest Number")
-form.st.sidebar.form(key="form", clear_on_submit=False)
-number_1=form.number.input("Enter the 1st number:",step=1)
-number_2=form.number.input("Enter the 2nd number:",step=1)
-number_3=form.number.input("Enter the 3rd number:",step=1)
-submitted=form.form_submit_button("Find the Largest")
-
-if submitted:
-  st.header('Result:')
-  result=largest_num(number_1,number_2,number_3)
-  st.Write(f'The largest number is {result}')
-else:
-  st.header('Result:')
-  st.write("Waiting for submiting")
+import streamlit as st 
+def largest_num(num1,num2,num3): 
+  result=0 
+  if num1>num2 and num1>num3: 
+    result=num1 
+  elif num2>num1 and num2>num3: 
+    result=num2 
+  else: 
+    result=num3 
+  return result 
+st.set_page_config(page_title='Largest number',layout='wide') 
+st.sidebar.title("Largest among three numbers") 
+form=st.sidebar.form(key="form",clear_on_submit=False) 
+num1=form.number_input('Enter the first number',step=1) 
+num2=form.number_input('Enter the second number',step=1) 
+num3=form.number_input('Enter the third number',step=1) 
+submitted=form.form_submit_button('Find the Largest') 
+if submitted: 
+  st.header('Result:') 
+  result=largest_num(num1,num2,num3) 
+  st.write(f'The largest number is {result}') 
+else: 
+  st.header('Result:') 
+  st.write('Waiting for submiting the numbers')
